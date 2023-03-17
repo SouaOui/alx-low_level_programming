c12ef0791fef0999f8fb36a069c2e0a598274146
SouaOui <soumiaouzat@gmail.com>
2023-03-17 15:54:13 -0700

    all alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
+
 /**
  * Entry point : main
  * Return : 0 (always success)
  *
-*/
+ */
 int main(void)
 {
 	char c;
+
+	/*this is a random comment*/
 
 	for (c = 'a'; c <= 'z' ; c++)
 	{
