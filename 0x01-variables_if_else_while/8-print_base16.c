ac7e0dd98076f87a6672852a58c2c8e28701ec96
SouaOui <soumiaouzat@gmail.com>
2023-03-17 19:53:05 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,13 @@
 #include<stdio.h>
 /**
  * main - Entry point
- *
+ * program that print all hexadecimal numbers
  * Return : Always 0 (Success)
- *
  */
 int main(void)
 {
 	int c;
 
-	/*
-	 * inverse letters
-	 */
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
@@ -21,5 +17,6 @@ int main(void)
 		putchar(c);
 	}
 	putchar('\n');
+	/* Return : 0 passed */
 	return (0);
 }
