f1196241c55ef1065c8f6a7291a9ca0b300fbe20
SouaOui <soumiaouzat@gmail.com>
2023-03-20 07:45:40 -0700

    two digit combine

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,10 @@ int main(void)
 	{
 		for (j = '1'; j <= '9'; j++)
 		{
-			putchar(i);
-			putchar(j);
+			if (i != j)
+			{	putchar(i);
+				putchar(j);
+			}
 			if (i != '8' || j != '9')
 			{
 				putchar(',');
