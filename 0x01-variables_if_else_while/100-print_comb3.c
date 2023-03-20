020d55553ef32fb1d7b46fd346f566f7fb703c5e
SouaOui <soumiaouzat@gmail.com>
2023-03-20 07:05:58 -0700

    combine two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/*
+ * function main() : Entry point
+ * Return : 0 always sucess
+ * this program print all smalesst combination of two digits
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	/*here is my code*/
+	for (i = '0'; i <= '8'; i++)
+	{
+		for (j = '1'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != '8' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
