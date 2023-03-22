c53eb70b2dc146c38fc060c95be70f981887da23
SouaOui <soumiaouzat@gmail.com>
2023-03-21 19:17:01 -0700

    last_digit_printed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* _print_last_digit - Computes the absolute value of an integer
-* @n: last digit
-* Return: last digit of the number in the parameter of the function
+* print_last_digit - Prints the last digit of a number
+* @n: The number to be treated
+* Return: Value of the last digit of number
 */
 int print_last_digit(int n)
 {
