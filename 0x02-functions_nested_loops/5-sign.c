cd33c8351c377c321b88798427cc83e6d8d87ea8
SouaOui <soumiaouzat@gmail.com>
2023-03-21 18:15:39 -0700

    sign number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - function to print signs
+ * @n: the character to check
+ * Return: 1 and print + if n is greater than zero. 0 equal. -1 less
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('0');
+return (0);
+}
