77c3157a85c203faa6a8a3473115b04a4e9a5dc0
SouaOui <soumiaouzat@gmail.com>
2023-03-22 19:47:46 -0700

    three five number

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,17 +1,18 @@
 #include "main.h"
-#include<stdio.h>
 /**
- * print_sum_below_1024 - print sum of multiple five and three numbers
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
-void print_sum_below_1024(void)
+int main(void)
 {
-	int i;
-	int sum = 0;
+	int i, y = 0;
 
-	for (i = 0; i < 1024; i++)
+	for (i = 0; i < 1024 ; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
-			sum += i;
+			y += i;
 	}
-	printf("sum is : %d\n", sum);
+	printf("%d\n", y);
+	return (0);
 }
