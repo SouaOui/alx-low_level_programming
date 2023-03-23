1f044a9126241ba65c66cd3d6a283fc2f91fd109
SouaOui <soumiaouzat@gmail.com>
2023-03-22 18:47:52 -0700

    table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,44 +4,30 @@
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
+
 void print_times_table(int n)
 {
-	int i, j, k;
+	int i, j, result;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
+			result = i * j;
+
+			if (j == 0)
+				printf("%d", j);
+			else if (result <= 9)
+				printf(",   %d", result);
+			else if (result > 9 && result < 100)
+				printf(",  %d", result);
+			else if (result > 99)
+				printf(", %d", result);
+
 		}
+		printf("\n");
 	}
 }
