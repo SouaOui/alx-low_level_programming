4f0da14657cccaf112532d362a7f1c3126ac646f
SouaOui <soumiaouzat@gmail.com>
2023-03-22 19:31:42 -0700

    multiple five and three sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sum_below_1024 - print sum of multiple five and three numbers
+ */
+void print_sum_below_1024(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("sum is : %d\n", sum);
+}
