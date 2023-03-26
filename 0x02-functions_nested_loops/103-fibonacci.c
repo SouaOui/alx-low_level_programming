21d0efae48d526c20378b67fba9c776840254370
SouaOui <soumiaouzat@gmail.com>
2023-03-26 15:08:30 -0700

    calculate even numbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,3 +1,27 @@
 #include<stdio.h>
 /**
- * main - this is a function to print the sum 
+ * main - Entry point
+ *
+ * Description: print the sum of even fibonacci number under 4000000
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long A = 0, B = 1, C, sum = 0;
+
+	for (i = 0; i < 40; i++)
+	{
+		C = A + B;
+		A = B;
+		B = C;
+
+		if (C % 2 == 0 && C < 4000000)
+		{
+			sum += C;
+		}
+	}
+	printf("sum is %lu\n", sum);
+	return (0);
+}
