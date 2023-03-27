07effd2645d9d194883f5886e12c2d5c06a4bede
SouaOui <soumiaouzat@gmail.com>
2023-03-27 14:29:36 -0700

    elements of an array printed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,12 +11,17 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n > 0)
+	{
 	for (i = 0; i <= (n - 1); i++)
 	{
 		printf("%d", *(a + i));
-		if (i == (n - 1))
+		if (i == (n - 1) || n == 1)
 			printf("\n");
 		else
 			printf(", ");
 	}
+	}
+	else
+		return;
 }
