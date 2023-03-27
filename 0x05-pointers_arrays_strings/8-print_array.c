674c0d321f9f60f558e5b34bcd61878fbde47111
SouaOui <soumiaouzat@gmail.com>
2023-03-27 14:35:29 -0700

    array of integers to be printed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,17 +11,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-	for (i = 0; i <= (n - 1); i++)
-	{
-		printf("%d", *(a + i));
-		if (i == (n - 1) || n == 1)
-			printf("\n");
-		else
+		printf("%d", a[i]);
+
+		if (i != n - 1)
 			printf(", ");
 	}
-	}
-	else
-		return;
+	printf("\n");
 }
