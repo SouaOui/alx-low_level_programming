417e23470cff8792997a202dee4ba54d94998f94
SouaOui <soumiaouzat@gmail.com>
2023-04-03 11:21:00 -0700

    find the first occurence letter

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - a function that locates a character in a string.
+ * @s: pointer to string
+ * @c: character to be located
+ * Return: returns the first occurrence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int len = strlen(s);
+
+	while (*s)
+	{
+		if (s[i] != c)
+			i++;
+		else
+			break;
+	}
+	if (i <= len - 1)
+		return (s + i);
+	else 
+		return (NULL);
+}	
