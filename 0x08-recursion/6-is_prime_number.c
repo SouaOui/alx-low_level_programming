a71da3a2bd53527f0adc1941a7c81542f0187e6c
SouaOui <soumiaouzat@gmail.com>
2023-04-06 19:48:22 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,7 +3,7 @@
  * _is_prime_test - function that test if a number is prime
  * @n: integer number
  * @i: integer number
- * Return : return 1 if a number is prime and 0 otherwise
+ * Return: return 1 if a number is prime and 0 otherwise
  */
 int _is_prime_test(int n, int i)
 {
@@ -12,7 +12,7 @@ int _is_prime_test(int n, int i)
 	if (n % i == 0)
 		return (0);
 	else
-		return(_is_prime_test(n, i + 1));
+		return (_is_prime_test(n, i + 1));
 }
 /**
  * is_prime_number - function that test if an input number is prime
@@ -22,8 +22,9 @@ int _is_prime_test(int n, int i)
 int is_prime_number(int n)
 {
 	int i = 2;
+
 	if (n <= 1)
 		return (0);
 	else
-		return(_is_prime_test(n, i));
+		return (_is_prime_test(n, i));
 }
