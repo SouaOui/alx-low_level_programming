ad615c07658728c164b401be9c2eac4bdf2e710c
SouaOui <soumiaouzat@gmail.com>
2023-04-08 14:33:51 -0700

    is this string palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * help_palindrome - function that check if a string is palindrome
+ * @str: pointer to string
+ * @left_index: integer value
+ * @right_index: integer value
+ * Return: 1 if the string is palindrome and 0 otherwise
+ */
+int help_palindrome(char *str, int left_index, int right_index)
+{
+	if (left_index >= right_index)
+		return (1);
+	else if (str[left_index] == str[right_index])
+		return (help_palindrome(str, left_index + 1, right_index - 1));
+	else
+		return (0);
+}
+/**
+ * is_palindrome - function that check if a string is palindrome
+ * using recursion
+ * @s: pointer to string
+ * Return: 1 if the string @s is palindrome and 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+	int len = 0;
+	int l, r;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	s -= len;
+	l = 0;	
+	r = len - 1;
+
+	if (l >= r)
+		return (1);
+	else if (help_palindrome(s, l, r))
+		return (1);
+	else
+		return (0);
+}
