764a4380b6cf04940a68989a498c05651b799d2b
SouaOui <soumiaouzat@gmail.com>
2023-04-13 16:30:11 -0700

    2._calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,12 @@
+#include "main.h"
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *array;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	array = malloc(size * nmemb);
+	if (array == NULL)
+		return (NULL);
+	return (array);
+}
