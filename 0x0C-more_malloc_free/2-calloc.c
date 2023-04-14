7140b0d4be85ee98fdca07622dbe8dfdd482676b
SouaOui <soumiaouzat@gmail.com>
2023-04-13 17:13:05 -0700

    2._calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,11 +8,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *array;
+	char *array1;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	array = malloc(size * nmemb);
 	if (array == NULL)
 		return (NULL);
+	array1 = array;
+	for (i = 0; i < (nmemb * size); i++)
+		array1[i] = '\0';
 	return (array);
 }
