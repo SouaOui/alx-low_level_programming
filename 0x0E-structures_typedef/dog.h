3ba601df83068a3d7078bf8667df040f9d91f242
SouaOui <soumiaouzat@gmail.com>
2023-04-17 13:23:30 -0700

    Header File

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dog - new type struct named dog
@@ -17,5 +18,11 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
