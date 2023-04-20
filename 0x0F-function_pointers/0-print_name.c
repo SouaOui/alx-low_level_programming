144eb600757bf07490eda1360b6b97efc5d9ca2b
SouaOui <soumiaouzat@gmail.com>
2023-04-19 17:33:06 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * f - function name
+ * @name: pointer to string
+ * Return: Nothing
+ */
+void f(char *name)
+{
+	printf("%s", name);
+}
+/**
+ * print_name - function that prints a name
+ * @name: pointer to string name to be printed
+ * @f: function pointer
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
