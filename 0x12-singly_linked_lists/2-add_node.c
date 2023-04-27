a8e8f156615ef23115bc70409d652a98df59965e
SouaOui <soumiaouzat@gmail.com>
2023-04-26 23:05:45 -0700

    2. Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - function that add a node
+ * @head: pointer to first element
+ * @str: string pointer
+ * Return: returns a pointer to first node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+}
