5d8f0e1289829685aabd23a0f4c83d0328ff7575
soumia <114097945+SouaOui@users.noreply.github.com>
2023-04-27 15:01:56 +0000

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -34,8 +34,5 @@ list_t *add_node_end(list_t **head, const char *str)
 			last_node = last_node->next;
 		last_node->next = new_node;
 	}
-	(*head)->next = new_node;
-	new_node->str = strdup(str);
-	new_node->next = NULL;
 	return (new_node);
 }
