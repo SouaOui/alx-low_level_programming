c096e7237051bfcc93ad455858b98973d2ab89c2
SouaOui <soumiaouzat@gmail.com>
2023-04-30 12:53:29 -0700

    5-free_listint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that free a list
+ * @head: pointer to first element
+ * Return : Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *save;
+
+	while ((*head)->next != NULL)
+	{
+		save = *head;
+		(*head) = (*head)->next;
+		free(save);
+	}
+	*head = NULL;
+}
