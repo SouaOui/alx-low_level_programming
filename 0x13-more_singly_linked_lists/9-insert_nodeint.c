85c2302176ff74876f79de977db4cdb2174c6ea8
SouaOui <soumiaouzat@gmail.com>
2023-05-03 06:35:05 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,33 @@
 #include"lists.h"
 /**
+ * commnets for your function goes here
+ */
+unsigned int num_node(listint_t *head)
+{
+	unsigned int len = 0;
+
+	while (head)
+	{
+		len++;
+		head = head->next;
+	}
+	return (len);
+}
+/**
+ * allocate_node - allocate memory for node
+ * @n: data of the node
+ * Return: adress of the node
+ */
+listint_t *allocate_node(int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	return (new_node);
+}
+/**
  * insert_nodeint_at_index - insert a node at position
  * @head: pointer to head of the list
  * @idx: position
@@ -8,29 +36,40 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current = *head;
-	unsigned int i = 0;
+	listint_t *new_node, *current;
+	unsigned int i = 0, len = 0;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
+	new_node = allocate_node(n);
+	if (*head == NULL)
+	{
+		*head = allocate_node(n);
+		return (*head);
+	}
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (*head);
 	}
-	while (i < idx - 1)
+	len = num_node(*head);
+	if (idx < len)
 	{
-		current = current->next;
-		i++;
+		current = *head;
+		while (i < idx - 1 && current->next != NULL)
+		{
+			current = current->next;
+			i++;
+		}
 	}
 	if (current != NULL)
 	{
 		new_node->next = current->next;
 		current->next = new_node;
-		new_node->n = n;
+	}
+	if (*head == NULL)
+	{
+		free(new_node);
+		return (NULL);
 	}
 	return (new_node);
 }
