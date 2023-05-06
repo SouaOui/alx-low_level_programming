d6fb48977ca1ae578cb6e0274ad41c7d9b585278
SouaOui <soumiaouzat@gmail.com>
2023-05-05 20:53:04 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,7 +8,8 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit_status;
-	if (index <= 31)
-		bit_status = (n >> index) & 1;
+	if (index > sizeof(n) * 8)
+		return (-1);
+	bit_status = (n >> index) & 1;
 	return (bit_status);
 }
