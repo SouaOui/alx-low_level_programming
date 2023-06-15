389bca99048a52ac556e41282c7c3408ee0d2059
SouaOui <soumiaouzat@gmail.com>
2023-06-15 03:50:05 -0700

    Print a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,9 +11,9 @@ size_t print_dlistint(const dlistint_t *h)
 
 	current = h;
 
-	while(current != NULL)
+	while (current != NULL)
 	{
-		len ++;
+		len++;
 		printf("%d\n", current->n);
 		current = current->next;
 	}
