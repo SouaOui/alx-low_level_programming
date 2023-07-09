139d31413d81df495a0c051cf47b1b8586878430
SouaOui <soumiaouzat@gmail.com>
2023-07-09 17:12:08 +0100

    add node to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,29 +9,28 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *current;
 
-    if (key == NULL || value == NULL || ht == NULL || strlen(key) == 0)
-        return (0);
-
-    index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index] && !(strcmp(ht->array[index]->key, key)))
-    {
-        ht->array[index]->value = strdup(value);
-        free(ht->array[index]->value);
-        return (1);
-    }
-    new_node = malloc(sizeof(hash_node_t));
+	if (key == NULL || value == NULL || ht == NULL || strlen(key) == 0)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+	while (current)
+	{
+		if (!strcmp(current->key, key))
+		{
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-    
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
-    free(new_node->value);
-    free(new_node->key);
-    free(new_node);
 	return (1);
 }
